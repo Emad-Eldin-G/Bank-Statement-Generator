f6443566ea1ae9a8a5f1f13649764962284ebe1b
Emad Gasser <omda.g@outlook.com>
2023-12-12 15:59:01 +0000

    done

--- zywa/api/urls.py
+++ zywa/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('pdf', getPdf, name='GetPdf'),
+    path('pdf', main, name='GetPdf'),
 ]
--- zywa/api/views.py
+++ zywa/api/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render
 from django.http import JsonResponse, HttpResponseForbidden
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import JsonResponse, HttpResponse
-from django.core.mail import send_mail, EmailMessage
+from django.core.mail import EmailMessage
 
 import os
 from django.conf import settings
 import json
 from datetime import datetime
 from docxtpl import DocxTemplate
+from docx2pdf import convert
+import smtplib
+from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from .models import transaction
+
 
 def invoiceGenerator(data):
     email = data['email']
@@ -29,7 +32,6 @@ def invoiceGenerator(data):
     for t in transactionsList:
         t[0] = t[0].strftime('%d/%m/%Y')
         total += t[3]
-    #print(transactionsList)
 
     #Rendering the template with the data fetched from database
     template.render({'email': email, 'total': total, 'fromDate': date1, 'toDate': date2, 'invoice_list': transactionsList})
@@ -37,22 +39,44 @@ def invoiceGenerator(data):
     return
 
 
+def sendEmail(data):
+    #In production, the email address and password will be fetched from environment variables
+    #address = os.environ.get('EMAIL_ADDRESS')
+    #password = os.environ.get('EMAIL_PASSWORD')
+    address = "omda.gasser@gmail.com"
+    #password = "" #for security reasons, the password will not be included in the code
+
+    #creating the email message
+    msg = EmailMessage()
+    msg['Subject'] = f'Zywa Invoice {data["date1"]} - {data["date2"]}'
+    msg['From'] = address
+    #msg['To'] = f"{data['email']}" #Will be sent to real email in production
+    msg['To'] = "omda.g@outlook.com" #For testing purposes
+    msg.set_content('Dear valued customer,\n\nPlease find your requested transaction details.\nThank you for using our services,\nTeam Zywa')
+
+    #converting the invoice from docx to pdf
+    convert(os.path.join(settings.BASE_DIR, f'api/invoices/{data["email"]}-{data["date1"]}-{data["date2"]}.docx'), os.path.join(settings.BASE_DIR, f'api/invoices/{data["email"]}-{data["date1"]}-{data["date2"]}.pdf'))
+
+    #reading the raw binary data from the invoice to attach it to the email
+    with open(os.path.join(settings.BASE_DIR, f'api/invoices/{data["email"]}-{data["date1"]}-{data["date2"]}.pdf'), 'rb') as f:
+        file_data = f.read()
+        file_name = f'{data["email"]}-{data["date1"]}-{data["date2"]}.pdf'
+        msg.add_attachment(file_data, maintype='application/pdf', subtype='octet-stream', filename=file_name)
+
+    #sending the email using the gmail smtp server
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(address, password)
+        smtp.send_message(msg)
+
+
 @api_view(['POST'])
-def getPdf(request):
+def main(request):
     data = json.loads(request.body)
-    #creation of invoice happens in invoiceGenerator func
+
+    #creation of invoice happens in invoiceGenerator service
     invoiceGenerator(data)
 
-    email = EmailMessage('Zywa Invoice', 
-        'Hello valued customer, \n Please find your requested transaction details. \n Thank you for using our services, \n Team Zywa',
-        'omda.g@outlook.com',
-        ["omda.gasser@gmail.com"], 
-        )
+    #sending email using the sendEmail service
+    sendEmail(data)
 
-    #email.attach_file(os.path.join(settings.BASE_DIR, f'api/invoices/{data["email"]}-{data["date1"]}-{data["date2"]}.pdf'), 'application/pdf')
-
-    try:
-        #email.send()
-        return Response("Good")
-    except:
-        return Response("Bad")
+    return JsonResponse({'message': 'Post request sent successfully'})
